cdf25c1ac7a5dff6009aadfc356e511e71ace2c1
Wei Wei <shazhouke@gmail.com>
2018-03-15 17:00:19 +0800

    Setup the project

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+ReactDOM.render(
+  <div>Hello World!</div>,
+  document.getElementById('container'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/index.jsx',
+    'react-hot-loader/patch',
+  ],
+  devtool: 'source-map',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js',
+  },
+  devServer: {
+    contentBase: './',
+    port: 9001,
+    publicPath: '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
