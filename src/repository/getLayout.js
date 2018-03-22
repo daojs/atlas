a1d3676434724977e50e6c43b864473195ab9ab8
HaipengWang <haip_wang@163.com>
2018-03-21 19:18:07 -0700

    use responsive container for section

--- src/repository/getLayout.js
+++ src/repository/getLayout.js
@@ -1,35 +1,44 @@
-import _ from 'lodash';
-import { GraphQLClient } from 'graphql-request';
+// import _ from 'lodash';
+import Promise from 'bluebird';
+// import { GraphQLClient } from 'graphql-request';
 
-const client = new GraphQLClient('/graphql');
+// const client = new GraphQLClient('/graphql');
 
-export function getLayout({
-  storyId,
-  sectionIds,
-}) {
-  return client.request(`query getLayout($storyId: String, $sectionIds: [String]) {
-    getLayout(storyId: $storyId, sectionIds: $sectionIds) {
-      i,
-      x,
-      y,
-      w,
-      h,
-    }
-  }`, {
-    storyId,
-    sectionIds,
-  }).then(data => data.getLayout);
+// export function getLayout({
+//   storyId,
+//   sectionIds,
+// }) {
+//   return client.request(`query getLayout($storyId: String, $sectionIds: [String]) {
+//     getLayout(storyId: $storyId, sectionIds: $sectionIds) {
+//       i,
+//       x,
+//       y,
+//       w,
+//       h,
+//     }
+//   }`, {
+//     storyId,
+//     sectionIds,
+//   }).then(data => data.getLayout);
+// }
+
+// export function setLayout({
+//   storyId,
+//   storyLayout,
+// }) {
+//   return client.request(`query setLayout($storyId: String, $storyLayout: [sectionLayoutInputType]) {
+//     setLayout(storyId: $storyId, storyLayout: $storyLayout)
+//   }
+//   `, {
+//     storyId,
+//     storyLayout: _.map(storyLayout, sectionLayout => _.pick(sectionLayout, ['x', 'y', 'w', 'h', 'i'])),
+//   });
+// }
+
+export function getLayout() {
+  return Promise.resolve([]);
 }
 
-export function setLayout({
-  storyId,
-  storyLayout,
-}) {
-  return client.request(`query setLayout($storyId: String, $storyLayout: [sectionLayoutInputType]) {
-    setLayout(storyId: $storyId, storyLayout: $storyLayout)
-  }
-  `, {
-    storyId,
-    storyLayout: _.map(storyLayout, sectionLayout => _.pick(sectionLayout, ['x', 'y', 'w', 'h', 'i'])),
-  });
+export function setLayout() {
+  return Promise.resolve();
 }
