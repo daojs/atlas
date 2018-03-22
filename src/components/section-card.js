c3180196c2562e08ff8fd957858df27983aae6a5
HaipengWang <haip_wang@163.com>
2018-03-22 01:55:36 -0700

    fix layout issue to prevent event from display

---
+++ src/components/section-card.js
@@ -0,0 +1,47 @@
+import React, { PureComponent } from 'react';
+import PropTypes, { any } from 'prop-types';
+import { Card } from 'antd';
+import _ from 'lodash';
+
+export default class SectionCard extends PureComponent {
+  render() {
+    return (
+      <div
+        {...this.props}
+      >
+        <Card
+          title={this.props.title}
+          style={{
+            cursor: 'move',
+          }}
+        >
+          <div
+            role="presentation"
+            onMouseDown={(e) => { e.stopPropagation(); }}
+            onMouseUp={(e) => { e.stopPropagation(); }}
+            style={{
+              cursor: 'point',
+            }}
+          >
+            {this.props.children}
+          </div>
+        </Card>
+      </div>
+    );
+  }
+}
+
+SectionCard.propTypes = {
+  children: PropTypes.arrayOf(any),
+  title: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  style: PropTypes.objectOf(any),
+};
+
+SectionCard.defaultProps = {
+  children: [],
+  title: '',
+  className: '',
+  style: {},
+};
