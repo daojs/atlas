55091862b16eb46fb0371c892aaa4cb298c54988
Yucong Jin <yucongj@microsoft.com>
2018-03-26 03:18:35 -0700

    dag usage-meal-card-bucket

--- src/dags/fetch-usage-meal-card-bucket.js
+++ src/dags/fetch-usage-meal-card-bucket.js
@@ -1,31 +1,48 @@
 export default function (client, simulation) {
   return (time, bestUser) => simulation
-    .then(({ recharge }) => client.call('query', recharge, {
-      aggregation: {
-        rechargeAmount: 'sum',
+    .then(({ recharge }) => client.call('dag', {
+      rechargeData: {
+        '@proc': 'read',
+        '@args': [
+          recharge,
+        ],
       },
-      filter: {
-        timestamp: {
-          type: 'time-range',
-          from: time.start,
-          to: time.end,
-        },
-        ...bestUser,
+      sumByTime: {
+        '@proc': 'query2',
+        '@args': [{
+          '@ref': 'rechargeData',
+        }, {
+          aggregation: {
+            rechargeAmount: 'sum',
+          },
+          filter: {
+            timestamp: {
+              type: 'time-range',
+              from: time.start,
+              to: time.end,
+            },
+            ...bestUser,
+          },
+          groupBy: {
+            customerId: 'value',
+          },
+        }],
       },
-      groupBy: {
-        customerId: 'value',
+      result: {
+        '@proc': 'query2',
+        '@args': [{
+          '@ref': 'sumByTime',
+        }, {
+          aggregation: {
+            customerId: 'count',
+          },
+          groupBy: {
+            rechargeAmount: {
+              type: 'bin',
+              step: 200,
+            },
+          },
+        }],
       },
-    }))
-    .then(id => client.call('query', id, {
-      aggregation: {
-        customerId: 'count',
-      },
-      groupBy: {
-        rechargeAmount: {
-          type: 'bin',
-          step: 200,
-        },
-      },
-    }).finally(() => client.call('remove', id)))
-    .then(id => client.call('read', id).finally(() => client.call('remove', id)));
+    }, 'result'));
 }
