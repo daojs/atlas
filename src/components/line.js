6cb083820b1192a935225d5061433d72d823298c
suro1995 <suro1995@gmail.com>
2018-03-27 13:33:19 +0800

    add comments

--- src/components/line.js
+++ src/components/line.js
@@ -2,6 +2,24 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import BaseChart from './base';
 
+/* Demo usage
+ <Line value={
+    source: [{
+      timestamp: '2018/1/1',
+      value: 10,
+    }, {
+      timestamp: '2018/1/2',
+      value: 20,
+    }],
+    // Axis dimension would be drawn as X axis.
+    // Although line chart only supports 1 axis dimension,
+    // we define the prop as array for better extensibility
+    axisDimensions: ['timestamp']
+    // Metric dimensions prop is optional.
+    // If not specified, all dimensions except axis dimensions would be used.
+    metricDimensions: ['value']
+  }/>
+*/
 export default class Line extends BaseChart {
   getSeriesOption() {
     const source = this.getSource();
