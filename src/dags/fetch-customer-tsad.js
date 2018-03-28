ff9ce0967cf54b1757c6e621a763c37cac1288e9
Yucong Jin <yucongj@microsoft.com>
2018-03-28 00:48:21 -0700

    server integration

--- src/dags/fetch-customer-tsad.js
+++ src/dags/fetch-customer-tsad.js
@@ -1,38 +1,30 @@
 import _ from 'lodash';
+import axios from 'axios';
 
-export default function (client, simulation) {
+export default function () {
   return (time, aggregation, bestUser) => {
     if (_.some([time, aggregation, bestUser], _.isNil)) {
       return Promise.resolve([]);
     }
 
-    return simulation
-      .then(({ transaction }) => client.call('dag', {
-        transactionData: {
-          '@proc': 'read',
-          '@args': [
-            transaction,
-          ],
-        },
-        result: {
-          '@proc': 'query2',
-          '@args': [{
-            '@ref': 'transactionData',
-          }, {
-            aggregation,
-            filter: {
-              timestamp: {
-                type: 'time-range',
-                from: time.start,
-                to: time.end,
-              },
-              ...bestUser,
+    return axios.post('/insight', {
+      '@proc': 'query',
+      '@args': [
+        'Transaction',
+        {
+          aggregation,
+          filter: {
+            timestamp: {
+              type: 'time-range',
+              from: time.start,
+              to: time.end,
             },
-            groupBy: {
-              timestamp: 'day',
-            },
-          }],
+          },
+          groupBy: {
+            timestamp: 'day',
+          },
         },
-      }, 'result'));
+      ],
+    }).then(({ data }) => data);
   };
 }
