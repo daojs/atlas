3d97d60ea3aa609a88c94f8f8751f34ff49902ff
Teng DU <tengdu@microsoft.com>
2018-03-30 02:36:09 +0800

    add revenue predication

--- src/master-kong/insight/layout.js
+++ src/master-kong/insight/layout.js
@@ -6,6 +6,7 @@ import './index.css';
 import StoryBoard from '../../story-board';
 import storyConfig from './story';
 import components from '../../components';
+import Usage from './usage-metric-trend-chart';
 
 const {
   // Bar,
@@ -18,6 +19,7 @@ const {
   SectionContainer,
   // Donut,
   // LineWithDataZoom,
+  // Line,
   SectionCard,
   StackBar,
 } = components;
@@ -39,6 +41,13 @@ export default function () {
           <div key="slicer">
             <Cell output="@time" label="" renderCell={WithComponent(TimeRange)} />
           </div>
+          <SectionCard
+            key="revenue-predicate"
+            title="总体销售额预测"
+            extra={<span>h1</span>}
+          >
+            <Cell renderCell={WithComponent(Usage)} />
+          </SectionCard>
           <SectionCard
             className="overall-analysis"
             key="given-branch"
---
+++ src/master-kong/insight/usage-metric-trend-chart.js
@@ -0,0 +1,115 @@
+// temp solution
+import ReactEcharts from 'echarts-for-react';
+import React, { PureComponent } from 'react';
+import moment from 'moment';
+import _ from 'lodash';
+
+function generatePredicateData(startDate, endDate) {
+  const list = {
+    员工餐厅: () => (Math.random() * 1000) + 26000,
+    北京小院: () => (Math.random() * 1000) + 5400,
+    咖喱屋: () => (Math.random() * 1000) + 2000,
+    自助餐厅: () => (Math.random() * 1000) + 3500,
+  };
+
+  const today = moment().startOf('day');
+  const start = moment(startDate).startOf('day');
+  const end = moment(endDate).startOf('day');
+  let day = start;
+
+  const minData = [];
+  const realData = [];
+  const maxData = [];
+
+  while (day.isBefore(today) || day.isSame(today)) {
+    const revenue = _.mapValues(list, func => func.call());
+    revenue.total = _.reduce(revenue, (prev, curr) => prev + curr);
+    revenue.date = day.format('YYYY-MM-DD');
+    realData.push(revenue);
+
+    if (day.isSame(today)) {
+      minData.push(revenue);
+      maxData.push(revenue);
+    } else {
+      minData.push(null);
+      maxData.push(null);
+    }
+    day = day.add(1, 'day');
+  }
+
+  while (day.isBefore(end)) {
+    const revenue = _.mapValues(list, func => func.call());
+    const minRevenue = _.mapValues(revenue, value => value * (1 - (Math.random() / 6)));
+    const maxRevenue = _.mapValues(revenue, value => value * (1 + (Math.random() / 6)));
+
+    revenue.total = _.reduce(revenue, (prev, curr) => prev + curr);
+    minRevenue.total =
+      _.reduce(minRevenue, (prev, curr) => prev + curr);
+    maxRevenue.total =
+      _.reduce(maxRevenue, (prev, curr) => prev + curr);
+    revenue.date = day.format('YYYY-MM-DD');
+    minRevenue.date = day.format('YYYY-MM-DD');
+    maxRevenue.date = day.format('YYYY-MM-DD');
+
+    realData.push(revenue);
+    minData.push(minRevenue);
+    maxData.push(maxRevenue);
+    day = day.add(1, 'day');
+  }
+
+  return { minData, realData, maxData };
+}
+
+const { minData, realData, maxData } = generatePredicateData('2018-03-21', '2018-04-15');
+
+export default class extends PureComponent {
+  xData = _.map(realData, i => i.date);
+  render() {
+    this.getOption = () => { // eslint-disable-line
+      return {
+        xAxis: {
+          type: 'category',
+          data: this.xData,
+        },
+        yAxis: {
+          type: 'value',
+        },
+        series: [{
+          data: _.map(realData, item => _.result(item, 'total'), null),
+          type: 'line',
+          smooth: true,
+        }, {
+          data: _.map(minData, item => _.result(item, 'total', null)),
+          type: 'line',
+          smooth: true,
+          lineStyle: {
+            type: 'dashed',
+          },
+        }, {
+          data: _.map(maxData, item => _.result(item, 'total', null)),
+          type: 'line',
+          smooth: true,
+          lineStyle: {
+            type: 'dashed',
+          },
+        }],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            label: {
+              backgroundColor: '#6a7985',
+            },
+          },
+        },
+      };
+    };
+
+    return (
+      <ReactEcharts
+        option={this.getOption()}
+        notMerge={true} //eslint-disable-line
+        {...this.props}
+      />
+    );
+  }
+}
