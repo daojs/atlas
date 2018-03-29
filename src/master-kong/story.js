5ed6dc0578cee83987e0584a38c4973245df0f80
Zhibin Liu <ljqx@live.cn>
2018-03-29 21:02:39 +0800

    [Master Kong] line chart

--- src/master-kong/story.js
+++ src/master-kong/story.js
@@ -3,14 +3,20 @@
 // import client from '../mock/worker';
 
 // dags
-// import factories from '../factories';
+import factories from '../factories';
+
+const {
+  fetchMasterKongRevenueAndVolumnTrend,
+} = factories;
 
 export default {
   parameters: {
     time: { default: { start: '2018-01-01', end: '2018-02-01' } },
   },
   cells: {
-
+    masterKongOverallRevenueAndVolumnTrend: {
+      factory: fetchMasterKongRevenueAndVolumnTrend(),
+    },
   },
   id: '10000',
 };
