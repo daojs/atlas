2011de0d311b4f56df1b4e6037267e4b44cb4fc4
Yucong Jin <yucongj@microsoft.com>
2018-03-29 04:03:46 -0700

    router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import React from 'react';
 import { Layout, Menu, Icon } from 'antd';
 import 'antd/dist/antd.css';
@@ -13,28 +14,41 @@ const contentStyle = {
   padding: 24,
 };
 
-ReactDOM.render(
-  (
+const SodexoLayout = () => (
+  <Layout>
     <Layout>
-      <Layout>
-        <Sider>
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span className="nav-text">最佳客户分析</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span className="nav-text">最佳菜品分析</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout style={{ backgroundColor: 'rgb(240, 242, 245)' }}>
-          <Content style={contentStyle}>
-            <BestCustomer />,
-          </Content>
-        </Layout>
+      <Sider>
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1">
+            <Icon type="user" />
+            <span className="nav-text">最佳客户分析</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="video-camera" />
+            <span className="nav-text">最佳菜品分析</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout style={{ backgroundColor: 'rgb(240, 242, 245)' }}>
+        <Content style={contentStyle}>
+          <BestCustomer />,
+        </Content>
       </Layout>
     </Layout>
+  </Layout>
+);
+
+const KsfLayout = () => (
+  <div>图片仅供参考</div>
+);
+
+ReactDOM.render(
+  (
+    <HashRouter >
+      <div>
+        <Route path="/sodexo" component={SodexoLayout} />
+        <Route path="/ksf" component={KsfLayout} />
+      </div>
+    </HashRouter >
   ), document.getElementById('bestcustomer'),
 );
