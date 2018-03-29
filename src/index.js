dbff8cb27d2039557ea4996a215dda3cf57f9d51
Ling Zhou <lizho@microsoft.com>
2018-03-29 05:53:15 -0700

    Add dashboard for masterkong

--- src/index.js
+++ src/index.js
@@ -1,46 +1,12 @@
 import ReactDOM from 'react-dom';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
-import { Layout, Menu, Icon } from 'antd';
 import 'antd/dist/antd.css';
 import _ from 'lodash';
 import './index.css';
-import BestCustomer from './best-customer/layout';
+import { SodexoLayout, MasterKongLayout } from './layout';
 
 _.templateSettings.interpolate = /{{([\s\S]+?)}}/g; // eslint-disable-line
-const { Content, Sider } = Layout;
-const contentStyle = {
-  background: '#f0f2f5',
-  padding: 24,
-};
-
-const SodexoLayout = () => (
-  <Layout>
-    <Layout>
-      <Sider>
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1">
-            <Icon type="user" />
-            <span className="nav-text">仪表盘</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="video-camera" />
-            <span className="nav-text">销售额预测</span>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout style={{ backgroundColor: 'rgb(240, 242, 245)' }}>
-        <Content style={contentStyle}>
-          <BestCustomer />,
-        </Content>
-      </Layout>
-    </Layout>
-  </Layout>
-);
-
-const MasterKongLayout = () => (
-  <div>图片仅供参考</div>
-);
 
 ReactDOM.render(
   (
