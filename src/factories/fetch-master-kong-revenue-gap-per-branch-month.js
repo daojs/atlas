0a74b9fdd52d5656861b7b20a336cac45c017c59
Zhibin Liu <ljqx@live.cn>
2018-03-29 22:52:27 +0800

    [Master Kong] heatmap

--- src/factories/fetch-master-kong-revenue-gap-per-branch-month.js
+++ src/factories/fetch-master-kong-revenue-gap-per-branch-month.js
@@ -1,5 +1,5 @@
 import revenueGap from '../../simulator-data/data/revenue-gap.json';
 
 export default function () {
-  return Promise.resolve(revenueGap);
+  return () => Promise.resolve(revenueGap);
 }
